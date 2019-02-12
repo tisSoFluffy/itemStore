71495cd711cb96414f96028d34c71f818f1d8671
Josh Fritz <joshua.l.fritz@gmail.com>
2019-02-12 10:03:36 -0600

    updasted .gitignore

--- code/app.py
+++ code/app.py
@@ -5,7 +5,6 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
